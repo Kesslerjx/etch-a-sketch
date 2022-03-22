42e40e1e05ee89dba5fcc11feeb785cc127d7426
Joseph Kessler <kesslerjx@icloud.com>
2022-03-22 12:37:03 -0400

    Add sketching functionality to grid

--- scripts.js
+++ scripts.js
@@ -1,14 +1,29 @@
 //Get the grid container
 const gridContainer = document.querySelector('.grid-container');
 
+//Get grid size input
+const gridSize = document.querySelector('#grid-size');
+
+//Setup clear button
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', clearGrid);
+
+//Setup change button
+const changeButton = document.querySelector('#set-grid-size');
+changeButton.addEventListener('click', changeGrid);
+
 //Draw grid
-drawGrid(20);
+drawGrid(50);
+
+function changeGrid() {
+    
+}
 
 function drawGrid(number) {
     //Get height and width of the squares
     let containerWidth = gridContainer.clientWidth; //Using clientWidth to not include border
-    let boxMargin = 2;
-    let borderWidth = 2;
+    let boxMargin = 1;
+    let borderWidth = 1;
     let sideLength = (containerWidth / number) - (boxMargin * 2);
     let numOfBoxes = number * number;
 
@@ -28,7 +43,21 @@ function drawGrid(number) {
         div.style.boxSizing = "border-box"; //Border box to account for padding and border
         div.setAttribute('id', i);
 
+        //Add event listener for hover
+        div.addEventListener('mouseover', function(event) {
+            //Change color of the box
+            event.target.style.backgroundColor = "black";
+        });
+
         //Append to grid container
         gridContainer.append(div);
     }
+}
+
+function clearGrid() {
+    //Get all boxes within the grid container
+    const boxes = gridContainer.querySelectorAll("*");
+
+    //Set color back to white
+    boxes.forEach(box => box.style.backgroundColor = "white");
 }
